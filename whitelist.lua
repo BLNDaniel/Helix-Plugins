c473ba83fc04ac04ce052af2bc64c69ca2fc3217
Criminal-Island_Danny <96847747+BLNDaniel@users.noreply.github.com>
2024-02-17 14:03:42 +0100

    Add files via upload

---
+++ whitelist.lua
@@ -0,0 +1,13 @@
+local whitelist = {
+    ["STEAM_0:1:455322465"] = true,
+    -- Add More Ids with ["STEAMID"] = true,
+}
+
+local function checkWhitelist(ply)
+    local steamid = ply:SteamID()
+    if not whitelist[steamid] then
+        ply:Kick("Du bist nicht gewhitelistet.")
+    end
+end
+
+hook.Add("PlayerInitialSpawn", "CheckWhitelist", checkWhitelist)
